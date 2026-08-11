16aafc2691938a45a3e81d700b26730202769a4c
Phoenix Jauregui <phoenix@feywickgames.com>
2026-08-11 17:28:37 -0500

    feat: Implement Cheer reaction behavior and resource

--- entity/character/_behavior/cheer_reaction_state.gd
+++ entity/character/_behavior/cheer_reaction_state.gd
@@ -1,2 +1,40 @@
 class_name CheerReactionState
 extends ReactionState
+
+var id: int
+var _qte_count: int = -1
+
+func update(_delta: float) -> State:
+	if not _character.animator.is_playing() and _reacted:
+		return CharacterIdleState.new()
+	if _success and not _reacted:
+		_reacted = true
+		_react()
+	return
+
+
+func _react() -> void:
+	super._react()
+	_character.animator.play_directional("idle", _character.facing)
+	_character.request_skill_text(_reaction.name)
+	_character.play_dialogue("cheer")
+
+
+func qte_succeeded(success: bool) -> void:
+	_qte_count += 1
+	if _qte_count == id:
+		super.qte_succeeded(success)
+
+
+func can_use(_skill_state: SkillState, actor: Character) -> bool:
+	if actor is Ally == _character is Ally:
+		var directions: Array[Vector2i] = [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]
+		var valid_tiles: Array[Vector2i]
+		for direction in directions:
+			var tile: Vector2i = _character.current_tile + direction
+			valid_tiles.append(tile)
+		
+		if actor.current_tile in valid_tiles:
+			if actor.facing == _character.facing:
+				return true
+	return false 
